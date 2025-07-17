e1cc9390cdd553ce4b6b753d54a02431f5ea17cf
lomichelotti <michelotti2k@gmail.com>
2025-07-17 00:04:20 -0300

    Add project files.

---
+++ Controllers/DocumentoController.cs
@@ -0,0 +1,18 @@
+﻿using GeradorDocumento;
+using Microsoft.AspNetCore.Mvc;
+
+namespace GerarDocumentoPDF.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class DocumentoController : Controller
+    {
+        [HttpGet(Name = "GerarDocumentoPdf")]
+        public IActionResult Get()
+        {
+            var documento = new GeradorDocumentoServico().GerarDocumentoPdf();
+
+            return File(documento, "application/pdf");
+        }
+    }
+}
